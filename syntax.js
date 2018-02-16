4a05b50b32f25fe213fcb0b97a5f1a132df959e5
maelle <maelle@seraphita.freeboxos.fr>
2018-02-16 18:21:17 +0100

    Library creation

--- syntax.js
+++ syntax.js
@@ -7,37 +7,35 @@
  */
 
 
-function syntax(){
-	//l'element contenant le texte
-	var codeElement = document.getElementById('colored');
-
-	/*les regex (commandes sql, variables sql, commentaires, et texte entre apostrophes)
-	* vous pouvez personnaliser la liste, il suffit d'ajouter votre mots suivi du caractére pipe.
-	*/
+var LibHighLigthSyntax = (function(){
+	
+	//create sql set
 	var sqlCmd = /\b(ADD|ALL|ALTER|AND|AS|BETWEEN|BY|CASE|CHECK|COLUMN|COMMENT|COUNT|CREATE|DATABASE|DELETE|ENUM|FLUSH|FOREIGN|FROM|GRANT|GROUP|IDENTIFIED|IF|INDEX|INNER|INSERT|IS|KEY|LIMIT|NOT|NULL|ON|OR|ORDER|OUTER|PRIMARY|PRIVILEGES|REFERENCES|SELECT|TABLE|TYPE|TO|UNIQUE|UPDATE|WHEN|WHERE)(?=[^\w])/g;
 	var sqlVar=/\b(bigint|bigserial|bit|bit varying|boolean|box|bytea|character varying|character|cidr|circle|date|double precision|inet|integer|interval|line|lseg|macaddr|money|numeric|path|point|polygon|real|smallint|serial|text|time|time with timezone|timestamp|timestamp (TZ)|tsquery|tsvector|txid_snapshot|uuid|xml)/g;
 	var multiLines  = /(\/\*.*\*\/)/g;
 	var inline=/(--(.+?)\n|--\n)/g;
 	var quoted=/('(.+)')/g;
-
-	// regex javascript
-	//specialJsReg = /\b(getElementsBy(TagName|ClassName|Name)|getElementById|typeof|instanceof)(?=[^\w])/g,
 	
-	//recupération du texte		
-	string = codeElement.innerHTML;
 	
-	/*coloration syntaxique
-	* attention à l'ordre!
-	* le script rajoute des span contenant des guillemets (<span class="sqlCmd">)
-	* si vous décidez de colorer du texte entre guillemets il faudra le faire au premier parse sinon le script prendra en compte le span!
-	*/
-	var parsed = string.replace(inline,'<span class="multilines">$1</span>');
-	parsed = parsed.replace(sqlVar,'<span class="sqlVar">$1</span>');
-	parsed = parsed.replace(sqlCmd,'<span class="sqlCmd">$1</span>');
-	parsed = parsed.replace(multiLines,'<span class="multilines">$1</span>');
-	parsed = parsed.replace(quoted,'<span class="quoted">$1</span>');
+	function getElem(id){
+		//l'element contenant le texte
+		this._id=id;
+		this._codeElement = document.getElementById(this._id);
+		this._string = this._codeElement.innerHTML;
+		this._parsed = this._string.replace(inline,'<span class="multilines">$1</span>');
+	this._parsed = this._parsed.replace(sqlVar,'<span class="sqlVar">$1</span>');
+	this._parsed = this._parsed.replace(sqlCmd,'<span class="sqlCmd">$1</span>');
+	this._parsed = this._parsed.replace(multiLines,'<span class="multilines">$1</span>');
+	this._parsed = this._parsed.replace(quoted,'<span class="quoted">$1</span>');
 	
 	//affichage du résultat
-	codeElement.innerHTML = parsed;
-}
-window.addEventListener('load',syntax,false);
+	this._codeElement.innerHTML = this._parsed;
+	
+	
+			
+	};
+	
+	return {'getElem':getElem}
+	
+})();
+
