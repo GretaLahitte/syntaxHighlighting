4ceb94ee0519e6d383bc052e68c73e47caa3de83
maelle <maelle@seraphita.freeboxos.fr>
2018-02-16 18:38:20 +0100

    english version

--- syntax.js
+++ syntax.js
@@ -1,9 +1,9 @@
 /*
- *  Ce script permet de colorer des mots clés définis par des expressions réguiliéres (regex)
- *  Il parse le texte à colorer, détecte les mots définies par les regex et les englobent dans une balise <span> avec une classe qui déterminera la couleur définie dans le css  
+ *  This library colorise SQL syntax in a html code tag.
+ *  You can add expression in the regex variables.
+ *  The getElem method parses the text in order to match a regular expression expanding to it a span tag (see syntax.css).
  *  ex:  <span class="sqlVar">CREATE</span>
- *  Ce code est simplifié au possible, vous pourrez par exemple définir une "classe" gérant la coloration avec des méthodes propres aux languages souhaité (javascript, sql, php, etc...)
- * 	Réalisé par M.LAHITTE le 17 mai 2017
+ *  by M.LAHITTE 2017
  */
 
 
@@ -18,18 +18,18 @@ var LibhighLigthSyntax = (function(){
 	
 	
 	function getElem(id){
-		//l'element contenant le texte
+		//text element
 		this._id=id;
 		this._codeElement = document.getElementById(this._id);
 		this._string = this._codeElement.innerHTML;
-		//traitement syntaxique, attention à l'ordre
+		//parse the text (the order is important...)
 		this._parsed = this._string.replace(inline,'<span class="multilines">$1</span>');
 		this._parsed = this._parsed.replace(sqlVar,'<span class="sqlVar">$1</span>');
 		this._parsed = this._parsed.replace(sqlCmd,'<span class="sqlCmd">$1</span>');
 		this._parsed = this._parsed.replace(multiLines,'<span class="multilines">$1</span>');
 		this._parsed = this._parsed.replace(quoted,'<span class="quoted">$1</span>');
 		
-		//affichage du résultat
+		//display the result
 		this._codeElement.innerHTML = this._parsed;			
 	};
 	
