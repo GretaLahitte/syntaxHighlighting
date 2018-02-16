46b2fe2790098ff3e80a1651f5601d09aab4084f
maelle <maelle@seraphita.freeboxos.fr>
2018-02-16 18:25:02 +0100

    indentation & Readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # syntaxHighlighting
-Use this script to color your code.
-Customize the regex to fit your needs
+This library allow syntax highlighting for your SQL syntax in HTML page.
+You can custom it to fit your needs.
+Add the syntax.css file in your header
+Point to the syntax.js file into your html file.
+To colorise an element represented an id, simply  call the getElem() méthode like this:
+
+var elemToColorise = new LibHighligthSyntax.getElem(the_elem_id); 
--- syntax.js
+++ syntax.js
@@ -7,9 +7,9 @@
  */
 
 
-var LibHighLigthSyntax = (function(){
+var LibhighLigthSyntax = (function(){
 	
-	//create sql set
+	//create sql syntax set
 	var sqlCmd = /\b(ADD|ALL|ALTER|AND|AS|BETWEEN|BY|CASE|CHECK|COLUMN|COMMENT|COUNT|CREATE|DATABASE|DELETE|ENUM|FLUSH|FOREIGN|FROM|GRANT|GROUP|IDENTIFIED|IF|INDEX|INNER|INSERT|IS|KEY|LIMIT|NOT|NULL|ON|OR|ORDER|OUTER|PRIMARY|PRIVILEGES|REFERENCES|SELECT|TABLE|TYPE|TO|UNIQUE|UPDATE|WHEN|WHERE)(?=[^\w])/g;
 	var sqlVar=/\b(bigint|bigserial|bit|bit varying|boolean|box|bytea|character varying|character|cidr|circle|date|double precision|inet|integer|interval|line|lseg|macaddr|money|numeric|path|point|polygon|real|smallint|serial|text|time|time with timezone|timestamp|timestamp (TZ)|tsquery|tsvector|txid_snapshot|uuid|xml)/g;
 	var multiLines  = /(\/\*.*\*\/)/g;
@@ -22,20 +22,17 @@ var LibHighLigthSyntax = (function(){
 		this._id=id;
 		this._codeElement = document.getElementById(this._id);
 		this._string = this._codeElement.innerHTML;
+		//traitement syntaxique, attention à l'ordre
 		this._parsed = this._string.replace(inline,'<span class="multilines">$1</span>');
-	this._parsed = this._parsed.replace(sqlVar,'<span class="sqlVar">$1</span>');
-	this._parsed = this._parsed.replace(sqlCmd,'<span class="sqlCmd">$1</span>');
-	this._parsed = this._parsed.replace(multiLines,'<span class="multilines">$1</span>');
-	this._parsed = this._parsed.replace(quoted,'<span class="quoted">$1</span>');
-	
-	//affichage du résultat
-	this._codeElement.innerHTML = this._parsed;
-	
-	
-			
+		this._parsed = this._parsed.replace(sqlVar,'<span class="sqlVar">$1</span>');
+		this._parsed = this._parsed.replace(sqlCmd,'<span class="sqlCmd">$1</span>');
+		this._parsed = this._parsed.replace(multiLines,'<span class="multilines">$1</span>');
+		this._parsed = this._parsed.replace(quoted,'<span class="quoted">$1</span>');
+		
+		//affichage du résultat
+		this._codeElement.innerHTML = this._parsed;			
 	};
 	
 	return {'getElem':getElem}
 	
 })();
-
